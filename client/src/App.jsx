691e544aed1280cdba7acd2984d64269de2c4e72
PramodyaMadushani <pramodya20madushani@gmail.com>
2025-05-01 17:18:07 +0530

    implement App.jsx,Avater.jsx,TopBar.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,32 +9,21 @@ import { ToastProvider } from "./components/ToastProvider";
 
 const App = () => {
     return (
-        <div className="pt-20">
-            <ToastProvider>
-                <Navbar />
-                <Routes>
-                    {/* Public Routes */}
-                    <Route path="/" element={<Login />} />
-
-                    {/* Protected Routes */}
-                    <Route element={<ProtectedRoute allowedRoles={["Admin", "User"]} />}>
-                        <Route path="/home" element={<Home />} />
-                        
-                    </Route>
-
-                    {/* Role-Specific Routes */}
-                    <Route element={<ProtectedRoute allowedRoles={["Admin"]} />}>
-                        <Route path="/admin" element={<AdminDashboard />} />
-                    </Route>
-
-                    
-
-                    
-
-                    
-                </Routes>
-            </ToastProvider>
-        </div>
+        <ThemeProvider>
+      <AuthProvider>
+      
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/admin" element={<Admin />} />
+          
+        </Routes>
+       
+      </AuthProvider>
+    </ThemeProvider>
     );
 };
 
