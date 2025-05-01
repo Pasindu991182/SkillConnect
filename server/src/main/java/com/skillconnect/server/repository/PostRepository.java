c290fa4d0a1049c0b03f0cbf9417ffa68f445f03
Pasindu991182 <pasindughp@gmail.com>
2025-05-02 00:13:05 +0530

    .

--- server/src/main/java/com/skillconnect/server/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skillconnect.server.repository;
-
-import com.skillconnect.server.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, int> {
-
-}
--- server/src/main/java/com/skillconnect/server/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skillconnect.server.repository;
-
-import com.lioc.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByEmail(String email);
-}
