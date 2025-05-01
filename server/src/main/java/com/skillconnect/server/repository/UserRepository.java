88084c9944af7596df5fca50cd2ceda2e7f3cef5
Vijini Jayathilaka <vijinijayathilaka24145@gmail.com>
2025-05-01 23:05:56 +0530

    repository

--- server/src/main/java/com/skillconnect/server/repository/UserRepository.java
+++ server/src/main/java/com/skillconnect/server/repository/UserRepository.java
@@ -1,5 +1,15 @@
 package com.skillconnect.server.repository;
 
-public class UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.skillconnect.server.model.User;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+    User findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+    List<User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
+
 }
+
