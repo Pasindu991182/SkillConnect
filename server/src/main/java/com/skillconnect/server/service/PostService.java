d124a5eda03e94edbfe4d25de922ca0bd9912914
Vijini Jayathilaka <vijinijayathilaka24145@gmail.com>
2025-05-01 23:00:43 +0530

    services

--- server/src/main/java/com/skillconnect/server/service/PostService.java
+++ server/src/main/java/com/skillconnect/server/service/PostService.java
@@ -1,5 +1,23 @@
 package com.skillconnect.server.service;
 
-public class PostService {
+import com.skillconnect.server.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostService {
+
+    Post createPost(Post post);
     
+    Optional<Post> findById(int postId);
+    
+    List<Post> findAllPosts();
+    
+    List<Post> findPostsByUserId(int userId);
+    
+    Post updatePost(Post post);
+    
+    void deletePost(int postId);
+
+    List<Post> loadFeed(int userId);
 }
